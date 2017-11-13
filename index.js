ae0fd861ea00043aaf43938e47b8a0604aab6587
Igor Morgunov <kekcuuuk@gmail.com>
2017-11-13 21:31:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-const app = "I don't do much."
+function bumpCounter() {
+  let counter = 0;
+
+  function addBump() {
+    counter++;
+  };
+
+  function getBumps() {
+    return counter;
+  };
+
+  return {
+    addBump,
+    getBumps
+  };
+};
+
+// function createAnimal(animalType) {
+//   function addDeadlyDevice(deadlyDevice) {
+//     return {
+//       animalType,
+//       deadlyDevice
+//     };
+//   };
+// };
