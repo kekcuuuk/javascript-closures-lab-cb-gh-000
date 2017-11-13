d49bdc2d82384a14d3cf24d413659fa6b24336f1
Igor Morgunov <kekcuuuk@gmail.com>
2017-11-13 21:32:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 // function bumpCounter() {
 //   let counter = 0;
-// 
+//
 //   function addBump() {
 //     counter++;
 //   };
-// 
+//
 //   function getBumps() {
 //     return counter;
 //   };
-// 
+//
 //   return {
 //     addBump,
 //     getBumps
