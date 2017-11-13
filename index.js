a9fa25c109f586f90f8ca9d890014095c9cd837a
Igor Morgunov <kekcuuuk@gmail.com>
2017-11-13 21:32:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-function bumpCounter() {
-  let counter = 0;
-
-  function addBump() {
-    counter++;
-  };
-
-  function getBumps() {
-    return counter;
-  };
-
-  return {
-    addBump,
-    getBumps
-  };
-};
+// function bumpCounter() {
+//   let counter = 0;
+// 
+//   function addBump() {
+//     counter++;
+//   };
+// 
+//   function getBumps() {
+//     return counter;
+//   };
+// 
+//   return {
+//     addBump,
+//     getBumps
+//   };
+// };
 
 function createAnimal(animalType) {
   function addDeadlyDevice(deadlyDevice) {
